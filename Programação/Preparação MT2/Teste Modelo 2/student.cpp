a90f792ec23ec768180c9dfcf5a6204389b5876e
GoncaloMatias1 <up202108703@up.pt>
2023-06-11 12:18:55 +0100

    .

--- Programação/Preparação MT2/Teste Modelo 2/student.cpp
+++ Programação/Preparação MT2/Teste Modelo 2/student.cpp
@@ -20,3 +20,23 @@ double Student::actual_grade() const{
     return 0.1 * ac_ + 0.45 * p1_ + 0.45 * p2_;
 }
 
+int main(){
+    { Student s("Andre Meira", "up201404877", 20, 18, 10);
+    cout << "[" << s.get_id() << "]" << "/";
+    cout << fixed << setprecision(2) << s.actual_grade() << endl; }
+    //[up201404877]/14.60
+    { Student s("John Doe", "up19790007", 20, 20, 20);
+    cout << "[" << s.get_id() << "]" << "/";
+    cout << fixed << setprecision(2) << s.actual_grade() << endl; }
+    //[up19790007]/20.00
+    { Student s("Graham Chapman", "up19790077", 10, 10, 10);
+    cout << "[" << s.get_id() << "]" << "/";
+    cout << fixed << setprecision(2) << s.actual_grade() << endl; }
+    //[up19790077]/10.00
+    { Student s("John Cleese", "up19790077", 8, 7, 13);
+    cout << "[" << s.get_id() << "]" << "/";
+    cout << fixed << setprecision(2) << s.actual_grade() << endl; }
+    //[up19790077]/9.80
+
+    return 0;
+}
